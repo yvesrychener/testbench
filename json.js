687fcec9ebc9252868b9395f46fe0fb39a014462
Yves Rychener <43433920+yvesrychener@users.noreply.github.com>
2020-12-07 11:57:49 +0100

    Update json.js

--- json.js
+++ json.js
@@ -1,6 +1,15 @@
-$().ready(function(){
-    $.getJSON( "/data.json", function( data ) {
-    console.log(data);
-    $("#text").html(data["text"]);
-  });
-});
+var req = XMLHttpRequest()
+req.addEventListener('load', onLoad);
+req.addEventListener('error', onFail);
+req.addEventListener('abort', onFail);
+req.open('GET', "https://yvesrychener.github.io/testbench/data.json");
+req.send();
+
+function onLoad(event) {
+    if (req.status >= 400) {
+      onFail(event);
+    } else {
+      var json = JSON.parse(this.responseText);
+      print(json);
+    }
+}
